2fc4dfd5e80030e5f067407dfb80ce04c3d55dff
suchiradi <suchirreddyadi@gmail.com>
2024-12-01 16:12:02 +0530

    list comprehension

---
+++ hackerrank.py/listcomprehension.py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+result = [[i, j, k]
+    for i in range(x + 1)   #checks if i is within the given constraint 
+    for j in range(y + 1)   #checks if j is within the given constraint 
+    for k in range(z + 1)   #checks if k is within the given constraint 
+    if i + j + k != n  ]
+
+# Print the result
+print(result)
