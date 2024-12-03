0b95e2d767faff85ad76bbd7f57c4b1580f667ad
suchiradi <suchirreddyadi@gmail.com>
2024-12-03 22:32:28 +0530

    swapcase

--- hackerrank.py/swapcase.py
+++ hackerrank.py/swapcase.py
@@ -1,14 +1,14 @@
-def swapcase(s):
-    result =" "
-    for i in s:
-        if i.islower():
-            result+=i.upper()
-        elif i.isupper():
-            result+=i.lower()
+def swapcase(s):  #s is a parameter
+    result =" "   #storing an empty string
+    for i in s:   #for loop to run through the entire string s
+        if i.islower():  #i.islower() checks if the letter is lower case. If it is lower case it will convert it into upper case.
+            result+=i.upper()  #the updated letter after conversion is stored in result
+        elif i.isupper(): #i.upper() checks if the letter is uppercase. If it is in uppercase ,it will convert it into lower case.
+            result+=i.lower()   #the updated letter is stored in the result 
         else:
-            result+=i
+            result+=i   #if other elements except letters are presented in the the string then it will remian unchanged
     return result
 
 s=input("Enter a string : \n")
-final=swapcase(s)
-print(final)    
+final=swapcase(s)  #free variable : stores the function into it
+print(final)       # free variable is called
