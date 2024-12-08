2d3ca3e09d104b8b4887e231a21c1fa40bfec427
suchiradi <suchirreddyadi@gmail.com>
2024-12-08 18:28:59 +0530

    stringsplit

---
+++ hackerrank.py/stringsplit.py
@@ -0,0 +1,4 @@
+line=input()  #taking input from the user 
+line=line.split(" ")  # converting the string into a list . ex- ["this","is","a","list"]
+line="-".join(line)   #joining the string with "-" in place of " "
+print(line)           # printing the updated line
